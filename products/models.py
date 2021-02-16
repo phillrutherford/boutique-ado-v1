5190c41cb8c002ee3dfad9d2e0e45a928613964e
phillrutherford <phillip.h.rutherford@gmail.com>
2021-02-16 18:42:35 +0000

    adding products section complete

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
 
-    class Meta: 
+    class Meta:
         verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=254)
